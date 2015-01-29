8c47afbda20198847638d4c7dd087464e0376d28
Mina Mikhail <mina@fightingtheboss.com>
2015-01-29 15:24:59 -0500

    Code we created together in last class of cohort 3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,21 +4,39 @@
 
 var POKEMON = {
   // Empty object is our placeholder for all of the fake Pokemon functions and properties
+  names: [],
+  types: [],
+  weaknesses: [],
+  descriptions: []
 };
 
 (function($) {
   $(document).ready(function() {
     // Generate fake Pokemon names
-    POKEMON.names = [];
-
     for (var i = 0; i < 100; i++) {
       POKEMON.names.push(faker.name.findName());
+      POKEMON.weaknesses.push(faker.lorem.words().join(', '));
+      POKEMON.types.push(faker.company.bsNoun());
+      POKEMON.descriptions.push(faker.lorem.paragraph());
     }
 
     // Add names to each Pokemon card
-    $('.pokemon').each(function(index) {
-      $(this).prepend('<h1>' + POKEMON.names[index] + '</h1>');
+    $('.pokemon-image').each(function(index) {
+      $(this).after('<h1>' + POKEMON.names[index] + '</h1>');
     });
+
+    $('.pokemon-weakness').each(function(index) {
+      $(this).html(POKEMON.weaknesses[index]);
+    });
+
+    $('.pokemon-type').each(function(index) {
+      $(this).html(POKEMON.types[index]);
+    });
+
+    $('.pokemon-description').each(function(index) {
+      $(this).html(POKEMON.descriptions[index]);
+    });
+
   });
 
   $('.logo').on('click', function(e) {
